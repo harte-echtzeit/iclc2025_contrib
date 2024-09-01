3821c579dc265c812092d8d2d1e68472ce08c8cc
harte_echtzeit <info@harte-echtzeit.com>
2024-09-01 11:22:42 +0200

    initial commit 2024-09-01

---
+++ paper/iclc-templates/README.md
@@ -0,0 +1,5 @@
+# iclc-templates
+
+A collection of templates for paper submissions, performance proposals, technical questionnaires and so on
+that have been used for ICLC over the years.
+
---
+++ petri_nets.hs
@@ -0,0 +1,5 @@
+-- some tests to work with petri nets
+
+place num max_num = if num == max_num then True else False
+
+and_trans input = if input == True then print "some action" else print "was false"
