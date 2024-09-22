d43a5741b73cc3946588450e7b5addc59aa68ec1
harte_echtzeit <info@harte-echtzeit.com>
2024-09-22 19:14:42 +0200

    abstract and code refinement 2024-09-22

--- petri_nets.hs
+++ petri_nets.hs
@@ -1,5 +1,14 @@
 -- some tests to work with petri nets
 
-place num max_num = if num == max_num then True else False
+-- old
+-- place_smpl num max_num = if num == max_num then True else False
+-- and_trans input = if input == True then print "some action" else print "was false"
+ 
+place num max_num
+  | num <= 0 = False
+  | num > max_num = False
+  | otherwise = True
 
-and_trans input = if input == True then print "some action" else print "was false"
+trans in_num in_state = if in_state then print(in_num - 1) else print(False)  
+
+
