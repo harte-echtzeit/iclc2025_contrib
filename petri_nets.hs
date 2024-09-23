d134f1679b77b741a9a408d522b0231464149332
harte_echtzeit <info@harte-echtzeit.com>
2024-09-23 21:27:23 +0200

    start preparing code example 2024-09-23

--- petri_nets.hs
+++ petri_nets.hs
@@ -4,11 +4,16 @@
 -- place_smpl num max_num = if num == max_num then True else False
 -- and_trans input = if input == True then print "some action" else print "was false"
  
-place num max_num
-  | num <= 0 = False
-  | num > max_num = False
+place in_num curr_num max_num
+  | in_num + curr_num <= 0 = False
+  | in_num + curr_num > max_num = False
   | otherwise = True
 
-trans in_num in_state = if in_state then print(in_num - 1) else print(False)  
+-- maybe leave out
+trans in_num in_state = if in_state then True else False  
 
+-- maybe use and perform change of curr_nums in places
+trans_num in_state = if in_state then 1 else 0
 
+-- example of an resulting place, parametrized (dummy wrapper)
+outP b = place (trans_num (place b b 3)) b 4
